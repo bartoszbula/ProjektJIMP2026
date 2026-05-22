95f536fe5711e34d1e33688031b1f8996c583310
bartoszbula <bartosz.bula@outlook.com>
2026-05-22 13:47:25 +0200

    Update Graph.java

--- Java/Graph.java
+++ Java/Graph.java
@@ -1,7 +1,10 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Graph {
     public Map<Integer, Node> nodes = new HashMap<>();
+    public List<double[]> edges = new ArrayList<>();
     public String error = "";
 }
